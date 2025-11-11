24eeb222ded1fe48bdcaae3d7498a5c36a0c56b5
quynhmai2808 <49148033+quynhmai2808@users.noreply.github.com>
2025-11-11 23:28:26 +0100

    Update dashboard.R

--- dashboard.R
+++ dashboard.R
@@ -71,3 +71,143 @@ map_UI <- function(id) {
   )
 }
 
+# SERVER ------------------------------------------------------------------
+
+map_SV <- function (id, data_by_group) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+    
+    # --- REPORT NAME: Update week list after selecting report_name
+    observeEvent(input$report_name, { # this function used for re-run, when user selects new
+      req(input$report_name)
+      
+      # List of weeks of selected input report name
+      weeks <- names(data_by_group[[input$report_name]])
+      
+      # If week is valid, then update UI
+      if (!is.null(weeks) && length(weeks) > 0) {
+      # # PMG Data
+      # if(input$report_name == "PMG"){ # e.x. User choose PMG
+      #   
+      #   # List of weeks of data_by_group$PMG
+      #   weeks <- names(data_by_group[["PMG"]])
+        
+        # Update dropdown "Week" by PMG List, select single week used for KPI Box value
+        updateSelectInput(session, "week", choices = weeks, selected = weeks[1])
+        
+        # Update multiple weeks selector, used for Trends
+        updateSelectizeInput(session, "selected_weeks", choices = weeks, selected = head(weeks,2))
+        
+      } else { # If week is invalid or no data, then reset input
+        
+        updateSelectInput(session, "week", choices = character(0))
+        updateSelectizeInput(session, "selected_weeks", choices = character(0))
+        
+      }
+    })
+    
+    # Here can be developed to adjust data performance by each report (if...eles). Because EAN Level only in PMG and Missing Item.
+    # OTP has no Total Sales, but Total G
+    
+    
+    # --- KPI Box data of single week
+    selected_data <- eventReactive(input$show_data, {
+      req(input$report_name, input$week)
+      df <- data_by_group[[input$report_name]][[as.character(input$week)]] 
+      
+      if (input$report_name == "PMG") {
+        df <- setNames(df, c("FileName", "Date", "Store", "ProductNr", "Description", "Sales", "Sticks", "Revenue", "Month")) 
+      } else if (input$report_name == "CIG") {
+        df <- setNames(df, c("FileName", "Date", "Store", "Sticks", "Revenue", "Month")) 
+      } else if (input$report_name == "ECIG_9961") {
+        df <- setNames(df, c("FileName", "Date", "Store", "Sales", "Revenue", "Month"))
+      } else if (input$report_name == "ECIG_9962") {
+        df <- setNames(df, c("FileName", "Date", "Store", "Sales", "Revenue", "Month"))
+      } else if (input$report_name == "ECIG_9963") {
+        df <- setNames(df, c("FileName", "Date", "Store", "Sales", "Revenue", "Month"))
+      } else if (input$report_name == "OTP") {
+        df <- setNames(df, c("FileName", "Date", "Store", "Total_G", "Revenue", "Month"))
+      } else {
+        df <- setNames(df, c("FileName", "Store", "ProductNr", "Description", "WGR", "Sales", "Revenue"))
+      }
+        
+    })
+    
+    output$kpi_sales <- renderValueBox({
+      df <- selected_data() 
+      valueBox(sum(df$Sales, na.rm = TRUE), "Total Sales", color = "blue")
+    })
+    
+    output$kpi_revenue <- renderValueBox({
+      df <- selected_data() 
+      valueBox(sum(df$Revenue, na.rm = TRUE), "Total Revenue", color = "green")
+    })
+    
+    output$kpi_item <- renderValueBox({
+      df <- selected_data() 
+      valueBox(n_distinct(df$ProductNr), "Total Item", color = "purple")
+    })
+    
+    output$kpi_store <- renderValueBox({
+      df <- selected_data() 
+      valueBox(n_distinct(df$Stores), "Stores count", color = "orange")
+    })
+    
+    output$table_data <- renderDataTable({
+      selected_data()
+    })
+    
+    # --- TREND Charts with multiple weeks
+    multi_week_data <- eventReactive(input$show_report, {
+      req(input$report_name, input$selected_weeks)
+      weeks <- as.character(input$selected_weeks)
+      data_lst <- data_by_group[[input$report_name]][weeks]
+      data_lst <- data_lst[!sapply(data_lst, is.null)]
+      
+      df_summary <- lapply(names(data_lst), function(wk) {
+        df <- data_lst[[wk]]
+        tibble(
+          Week = as.numeric(wk),
+          Total_Sales = sum(df$Sales, na.rm = TRUE),
+          Total_Revenue = sum(df$Revenue, na.rm = TRUE),
+          Item_count = n_distinct(df$ProductNr),
+          Stores_count = n_distinct(df$Store),
+          
+        )
+      }) %>% dplyr::bind_rows()
+      
+      df_summary
+    })
+    
+    # --- Heatmap ----
+    output$heatmap <- renderPlotly({
+      
+    })
+    
+    # --- Trend plots ---
+    output$plot_sales <- renderPlotly({
+      df <- multi_week_data()
+      p <- ggplot(df, aes(x = Week, y = Total_Sales)) + geom_line() + geom_point() + theme_minimal() + labs(y = "Total Sales", x = "Week")
+      plotly::ggplotly(p)
+    })
+  
+    output$plot_revenue <- renderPlotly({
+      df <- multi_week_data()
+      p <- ggplot(df, aes(x = Week, y = Total_Revenue)) + geom_line() + geom_point() + theme_minimal() + labs(y = "Total Revenue", x = "Week")
+      plotly::ggplotly(p)
+    })
+    
+    output$plot_stores_count <- renderPlotly({
+      df <- multi_week_data()
+      p <- ggplot(df, aes(x = Week, y = Stores_count)) + geom_line() + geom_point() + theme_minimal() + labs(y = "Store count", x = "Week")
+      plotly::ggplotly(p)
+    })
+    
+    output$plot_item_count <- renderPlotly({
+      df <- multi_week_data()
+      p <- ggplot(df, aes(x = Week, y = Item_count)) + geom_line() + geom_point() + theme_minimal() + labs(y = "Total Item", x = "Week")
+      plotly::ggplotly(p)
+    })
+    
+  })
+}
