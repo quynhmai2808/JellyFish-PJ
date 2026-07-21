317471ed1c45f4bfbe7e0c4722c858fad3fce8e3
quynhmai2808 <49148033+quynhmai2808@users.noreply.github.com>
2026-07-21 09:21:52 +0200

    Create source.R

---
+++ project_template/source.R
@@ -0,0 +1,8 @@
+# Library
+source('/qa/data1/_Project_Template/Code/libraries.R')
+
+# Data --------------------------------------------------------------------
+source('/qa/data1/_Project_Template/Code/data.R')
+
+# Modules -----------------------------------------------------------------
+source('/qa/data1/_Project_Template/Code/dashboard.R')
